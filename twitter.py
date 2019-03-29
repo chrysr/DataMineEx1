cd86d78bd355904ec5310032bd42bab666c4ae42
chrysr <sdi1500205@di.uoa.gr>
2019-03-29 14:58:51 +0200

    up to date

--- twitter.py
+++ twitter.py
@@ -8,8 +8,20 @@ text=input.read()
 text=text.lower();
 
 text=re.sub(r'http\S+', '', text)
-text=re.sub('@[a-zA-Z0-9]+','USER ',text)
+text=re.sub('@[a-zA-Z0-9_]+','USER',text)
 text=text.translate(None,s.punctuation)
+dict={}
+start=0
+end=0
+tempstr=''
+i=10
+while(i>0):
+    place1=text.find("\t",start,len(text))
+    print text[start:place1]
+    #place2=text.find("\t",place1,len(text))
+    #print text[start:place2]
+    start=place1+1
+    i-=1
 
 tokens=nltk.word_tokenize(text)
 
