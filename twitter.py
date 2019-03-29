a4dfc59e682237e29137f7d8e60e90427bc3b6d7
chrysr <sdi1500205@di.uoa.gr>
2019-03-29 14:39:08 +0200

    aded stuff

--- twitter.py
+++ twitter.py
@@ -1,1 +1,18 @@
 import nltk
+import string as s
+import re
+
+input=open("train2017.tsv",'r')
+output=open("out.tsv",'w')
+text=input.read()
+text=text.lower();
+
+text=re.sub(r'http\S+', '', text)
+text=re.sub('@[a-zA-Z0-9]+','USER ',text)
+text=text.translate(None,s.punctuation)
+
+tokens=nltk.word_tokenize(text)
+
+output.write(text)
+input.close()
+output.close()
